f1773e9cc06fc5593c6a22b4f982e1b2b7e2fc87
piamsden <133517985+piamsden@users.noreply.github.com>
2023-05-13 09:45:46 -0700

    Update randomvid.js

--- js/randomvid.js
+++ js/randomvid.js
@@ -6,9 +6,9 @@ let video = new Array();
 
 //higher
 video.push({
-    video_id: 'VZZl5F-u03Q',
-    video_name: "destroy lonely - bane (sped up + reverb)",
-    id: 'VZZl5F-u03Q' + videoSettings + 'start=0',
+    video_id: '-OrpJshG_GI',
+    video_name: "lil baby- pure cocaine",
+    id: '-OrpJshG_GI' + videoSettings + 'start=0',
     note: '',
     volume: 35,
 });
